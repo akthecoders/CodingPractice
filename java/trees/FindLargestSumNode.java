62d2c06b10600fe7de977a1c4b810f8d4a84c14f
Akshay Kumar <akthecoder@gmail.com>
2019-11-28 11:02:22 +0530

    FindLargestSumNode

---
+++ java/trees/FindLargestSumNode.java
@@ -0,0 +1,12 @@
+package trees;
+
+/**
+ * FindLargestSumNode
+ */
+public class FindLargestSumNode {
+  public static TreeNode<Integer> findLargestSumNode(TreeNode<Integer> root) {
+    TreeNode<Integer> result = null;
+    
+    return result;
+  }
+}
