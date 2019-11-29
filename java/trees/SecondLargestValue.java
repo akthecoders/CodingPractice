0b2fe08fe6886f3bf4f348b6b6c0bedc5171ecca
Akshay Kumar <akthecoder@gmail.com>
2019-11-29 15:01:03 +0530

    Second Largest Completed

--- java/trees/SecondLargestValue.java
+++ java/trees/SecondLargestValue.java
@@ -17,26 +17,6 @@ public class SecondLargestValue {
   private static L2Node helper(TreeNode<Integer> root) {
     L2Node result = new L2Node();
     result.largest = root;
-    // for(int i = 0; i < root.children.size(); i++) {
-    //   TreeNode<Integer> tNode = root.children.get(i);
-    //   if(result.largest == null) {
-    //     result.largest = tNode;
-    //   } 
-    //   else {
-    //     if(result.largest.data < tNode.data) {
-    //         result.secondLargest = result.largest;
-    //         result.largest = tNode;
-    //     }
-    //     else {
-    //       if(result.secondLargest == null) {
-    //         result.secondLargest = tNode;
-    //       }
-    //       else if(result.secondLargest.data < tNode.data) {
-    //         result.secondLargest = tNode;
-    //       }
-    //     }
-    //   }
-    // }
 
     for(int i = 0; i < root.children.size(); i++) {
       L2Node tNode = helper(root.children.get(i));
@@ -45,7 +25,7 @@ public class SecondLargestValue {
       } 
       else {
         if(result.largest.data < tNode.largest.data) {
-            if(result.largest.data < tNode.secondLargest.data) {
+            if(tNode.secondLargest != null && result.largest.data < tNode.secondLargest.data) {
               result.secondLargest = tNode.secondLargest;
             }
             else {
