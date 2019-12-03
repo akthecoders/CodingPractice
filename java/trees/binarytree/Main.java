1da77c758694a81a9d3fadb77351a3c1332c65f3
Akshay Kumar <akthecoder@gmail.com>
2019-12-03 11:36:34 +0530

    SerializeDeserialize

--- java/trees/binarytree/Main.java
+++ java/trees/binarytree/Main.java
@@ -10,8 +10,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         BinaryNode<Integer> root = InputLevelWise.inputLevelWise(scan);
+<<<<<<< HEAD
         PrintLevelWise.printLevelWise(root);
         // System.out.println("No of nodes : " + CountNoOfNodes.countNoOfNodes(root));
         System.out.println("Diameter of tree : " + DiameterBTree.getDiameter(root));
+=======
+        // PrintLevelWise.printLevelWise(root);
+        // System.out.println("No of nodes : " + CountNoOfNodes.countNoOfNodes(root));
+        String str = SerializeDeserialize.serialize(root);
+        System.out.println("String : " + str);
+        BinaryNode<Integer> tree = SerializeDeserialize.deserialize(str);
+        if(tree == null) {
+          System.out.println("Null Tree");
+        }
+        else {
+          PrintLevelWise.printLevelWise(tree);
+        }
+>>>>>>> f1005a9... SerializeDeserialize
     }
 }
--- java/trees/binarytree/SerializeDeserialize.java
+++ java/trees/binarytree/SerializeDeserialize.java
@@ -1,19 +1,66 @@
 package trees.binarytree;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * SerializeDeserialize
  */
 public class SerializeDeserialize {
 
  public static String serialize(BinaryNode<Integer> root) {
+   if(root == null) {
+     return "";
+   }
   String result = "";
-  result +=  root.data + "[" + root.left.data + "|" + root.right.data + "],";
+  result +=  root.data + ":";
+  if(root.left != null) {
+    result += root.left.data;
+  }
+  else {
+    result += "0";
+  } 
+  result += "|";
+  if(root.right != null) {
+    result += root.right.data;
+  }
+  else {
+    result += "0";
+  } 
+  result += ",";
   result += serialize(root.left);
   result += serialize(root.right);
   return result;
  } 
 
- public static void deserialize(BinaryNode<Integer> root) {
+ public static BinaryNode<Integer> deserialize(String inputString) {
+  String[] arrOfString = inputString.split(",");
+  Map<Integer, BinaryNode<Integer>> map = new HashMap<Integer,BinaryNode<Integer>>();
+  BinaryNode<Integer> mNode = null;
+  for(String node : arrOfString) {
+    String[] arrNode = node.split(":");
+    Integer pNode = Integer.parseInt(arrNode[0]);
+    String childNodeArray[] = arrNode[1].split("|");
+    Integer leftChild = Integer.parseInt(childNodeArray[0]);
+    Integer rightChild = Integer.parseInt(childNodeArray[1]);
    
+    if(map.containsKey(pNode)) {
+      mNode = map.get(pNode);
+    }
+    else {
+      mNode = new BinaryNode<Integer>(pNode);
+    }
+    if(leftChild > 0) {
+      BinaryNode<Integer> leftChildNode = new BinaryNode<Integer>(leftChild);
+      map.put(leftChild, leftChildNode);
+      mNode.left = leftChildNode;
+    }
+    if(rightChild > 0) {
+      BinaryNode<Integer> rightChildNode = new BinaryNode<Integer>(rightChild);
+      map.put(rightChild, rightChildNode);
+      mNode.right = rightChildNode;
+    }
+  }
+  return mNode;
  }
 }
