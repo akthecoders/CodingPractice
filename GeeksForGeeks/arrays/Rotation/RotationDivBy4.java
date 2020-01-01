e2901e2ad669e99de95ae5514e3817cae8734f08
Akshay Kumar <akthecoder@gmail.com>
2020-01-01 20:50:18 +0530

    RotationDivBy4

---
+++ GeeksForGeeks/arrays/Rotation/RotationDivBy4.java
@@ -0,0 +1,40 @@
+package Rotation;
+
+/**
+ * RotationDivBy4
+ */
+public class RotationDivBy4 {
+
+    static int countRotations(String n) {
+        int len = n.length();
+
+        if (len == 1) {
+            int oneDigit = n.charAt(0) - '0';
+
+            if (oneDigit % 4 == 0)
+                return 1;
+
+            return 0;
+        }
+
+        int twoDigit, count = 0;
+        for (int i = 0; i < (len - 1); i++) {
+            twoDigit = (n.charAt(i) - '0') * 10 + (n.charAt(i + 1) - '0');
+
+            if (twoDigit % 4 == 0)
+                count++;
+        }
+
+        twoDigit = (n.charAt(len - 1) - '0') * 10 + (n.charAt(0) - '0');
+
+        if (twoDigit % 4 == 0)
+            count++;
+
+        return count;
+    }
+
+    public static void main(String args[]) {
+        String n = "4834";
+        System.out.println("Rotations: " + countRotations(n));
+    }
+}
