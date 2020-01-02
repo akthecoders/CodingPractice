df4eb2bb593d9963e7b66ac735445a3e9295733c
Akshay Kumar <akthecoder@gmail.com>
2020-01-02 21:51:37 +0530

    PairInSortedRotatedArray

---
+++ GeeksForGeeks/arrays/Rotation/PairInSortedRotatedArray.java
@@ -0,0 +1,40 @@
+package Rotation;
+
+/**
+ * PairInSortedRotatedArray
+ */
+public class PairInSortedRotatedArray {
+
+    public static void main(String[] args) {
+        int arr[] = {11, 15, 6, 8, 9, 10}; 
+        int sum = 16; 
+        int n = arr.length;
+        if (pairInSortedRotated(arr, n, sum))
+            System.out.print("Array has two elements" + " with sum 16");
+        else
+            System.out.print("Array doesn't have two" + " elements with sum 16 "); 
+
+    }
+
+    private static boolean pairInSortedRotated(int[] arr, int n, int sum) {
+        int i;
+        for(i = 0; i < n; i++) {
+            if(arr[i] > arr[i+1])
+            break;
+        }
+        int l = (i + 1) % n;
+        int r = i;
+        while(l != r) {
+            if(arr[l] + arr[r] == sum) {
+                return true;
+            }
+            if(arr[l] + arr[r] < sum) {
+                l = (l + 1) % n;
+            }
+            else {
+                r = (n + r - 1) % n;
+            }
+        }
+        return false;
+    }
+}
