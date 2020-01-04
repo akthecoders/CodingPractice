474f2d3d0ae42505d6671fa1398f1212f4ef227f
Akshay Kumar <akthecoder@gmail.com>
2020-01-04 20:53:05 +0530

    SickLeave

---
+++ GeeksForGeeks/Misc/SickLeave.java
@@ -0,0 +1,9 @@
+/**
+ * SickLeave
+ */
+public class SickLeave {
+
+    public static void main(String[] args) {
+        System.out.println("Suffering from severe cold! Sick leave of today :( ");
+    }
+}
