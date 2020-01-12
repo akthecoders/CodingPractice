ef46a829c8657d9ad5c1798214aa51fe95296625
Akshay Kumar <akthecoder@gmail.com>
2020-01-12 22:24:38 +0530

    NextGreaterElement

---
+++ GeeksForGeeks/arrays/OrderStatistics/NextGreaterElement.java
@@ -0,0 +1,75 @@
+package OrderStatistics;
+
+/**
+ * NextGreaterElement
+ */
+public class NextGreaterElement {
+
+    public static void main(String[] args) {
+        int arr[] = { 11, 13, 21, 3 };
+        int n = arr.length;
+        printNGE(arr, n);
+    }
+
+    public static void printNGE(int[] arr, int n) {
+        int i = 0;
+        stack s = new stack();
+        s.top = -1;
+        int element, next;
+        s.push((arr[0]));
+
+        for(i = 0; i< n; i++) {
+            next = arr[i];
+            if(s.isEmpty() == false) {
+                element = s.pop();
+                while(element < next) {
+                    System.out.println(element + "-->" + next);
+                    if(s.isEmpty() == true) {
+                        break;
+                    }
+                    element = s.pop();
+                }
+                if(element > next) {
+                    s.push(element);
+                }
+            }
+
+            s.push(next);
+        }
+
+        while (s.isEmpty() == false)  
+        { 
+            element = s.pop(); 
+            next = -1; 
+            System.out.println(element + " -- " + next); 
+        } 
+    }
+
+    static class stack {
+        int top;
+        int items[] = new int[100];
+
+        void push(int x) {
+            if (top == 99) {
+                System.out.println("Stack full");
+            } else {
+                items[++top] = x;
+            }
+        }
+
+        int pop() {
+            if (top == -1) {
+                System.out.println("Underflow error");
+                return -1;
+            } else {
+                int element = items[top];
+                top--;
+                return element;
+            }
+        }
+
+        boolean isEmpty() {
+            return (top == -1) ? true : false;
+        }
+    }
+}
