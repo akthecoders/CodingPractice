2fc1f8cfc3a2afabea78e59d449a5746e3962005
Akshay Kumar <akthecoder@gmail.com>
2020-01-14 23:44:25 +0530

    MOAlgorithm

--- GeeksForGeeks/arrays/RangeQueries/MOAlgorithm.java
+++ GeeksForGeeks/arrays/RangeQueries/MOAlgorithm.java
@@ -11,6 +11,38 @@ public class MOAlgorithm {
         moAlgorithm(a,q); 
     }
 
-    public static void moAlgorithm(int[] arr, int[][] query) {}
-    
+    public static void moAlgorithm(int[] arr, int[][] query) {
+      int currL = 0;
+      int currR = 0;
+      int  L = 0;
+      int R = 0;
+      int sum = 0;
+      for(int i = 0; i < query.length; i++) {
+        L = query[i][0];
+        R = query[i][1];
+
+        while(currL < L) {
+          sum -=  arr[currL];
+          currL++;
+        }
+
+        while(currL > L) {
+          sum += arr[currL - 1];
+          currL--;
+        }
+
+        while(currR <= R) {
+          sum += arr[currR];
+          currR++;
+        }
+
+        while (currR > R+1) 
+        { 
+            sum -= arr[currR-1]; 
+            currR--; 
+        }
+
+        System.out.println("Sum of [" + L + ", " + R + "] is" + sum);
+      }
+    }    
 }
