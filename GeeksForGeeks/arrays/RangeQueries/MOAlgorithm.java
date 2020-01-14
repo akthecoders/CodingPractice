6303d9b90375b53690f9b1a3b21eb96bc7e1debe
Akshay Kumar <akthecoder@gmail.com>
2020-01-14 23:28:10 +0530

    MO Algorithm

---
+++ GeeksForGeeks/arrays/RangeQueries/MOAlgorithm.java
@@ -0,0 +1,16 @@
+package RangeQueries;
+
+/**
+ * MOAlgorithm
+ */
+public class MOAlgorithm {
+
+    public static void main(String[] args) {
+        int a[] = {1, 1, 2, 1, 3, 4, 5, 2, 8}; 
+        int q[][] = {{0, 4}, {1, 3}, {2, 4}}; 
+        moAlgorithm(a,q); 
+    }
+
+    public static void moAlgorithm(int[] arr, int[][] query) {}
+    
+}
