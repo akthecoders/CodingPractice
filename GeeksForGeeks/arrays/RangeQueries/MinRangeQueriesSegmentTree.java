d537dff595a653901bf3c2658858dcbee80cc7d8
Akshay Kumar <akthecoder@gmail.com>
2020-01-16 08:29:10 +0530

    MinRangeQueriesSegmentTreev2

--- GeeksForGeeks/arrays/RangeQueries/MinRangeQueriesSegmentTree.java
+++ GeeksForGeeks/arrays/RangeQueries/MinRangeQueriesSegmentTree.java
@@ -4,57 +4,44 @@ package RangeQueries;
  * MinRangeQueriesSegmentTree
  */
 public class MinRangeQueriesSegmentTree {
-
-    static class Query { 
-        int L, R; 
-    
-        public Query(int L, int R) 
-        { 
-            this.L = L; 
-            this.R = R; 
-        } 
-    };
-
     public static void main(String[] args) {
-        int a[] = {7, 2, 3, 0, 5, 10, 3, 12, 18}; 
+        int a[] = {1, 0, 2, 5, 12, 3}; 
         int n = a.length; 
-        Query q[] = {new Query(0, 4),  
-                    new Query(4, 7),  
-                    new Query(7, 8)}; 
-        int tree[] = new int[20];
-        buildTree(a, tree, 0, n - 1, 1);
+        int tree[] = new int[30];
+        buildTree(a, tree, 0, n - 1, 0);
         for (int i : tree) {
             System.out.print( i + " ");
         }
         System.out.println();
-        System.out.println("Min : " + query(1, 0, n - 1, 7, 8, tree));
+        System.out.println("Min : " + query(0, 0, n - 1, 3, 5, tree));
     }
 
     public static void buildTree(int[] arr, int[] tree, int start, int end, int node) {
+        System.out.println(start + "-" + end + "-" + node);
         if(start == end) {
             tree[node] = arr[start];
             return;
         }
-
         int mid = (start + end ) / 2;
 
-        buildTree(arr, tree, start, mid, 2 * node);
-        buildTree(arr, tree, mid + 1, end, 2 * node + 1);
-        int leftMin = tree[2 * node];
-        int rightMin = tree[2 * node + 1];
+        buildTree(arr, tree, start, mid, 2 * node + 1);
+        buildTree(arr, tree, mid + 1, end, 2 * node + 2);
+        int leftMin = tree[2 * node + 1];
+        int rightMin = tree[2 * node + 2];
         tree[node] = leftMin < rightMin ? leftMin : rightMin;
+        return;
     }
 
     public static int query(int node, int start, int end, int l , int r, int[] tree) {
-        if(end < l || start > r) {
-            return 1;
+        if(r < start || l > end) {
+            return Integer.MAX_VALUE;
         }
         if( l <= start && r >= end) {
             return tree[node];
         }
         int mid = (start + end) / 2;
-        int leftMin = query(2 * node, start, mid, l, r, tree);
-        int rightMin = query(2 * node + 1 , mid + 1, end, l, r, tree);
+        int leftMin = query(2 * node + 1, start, mid, l, r, tree);
+        int rightMin = query(2 * node + 2 , mid + 1, end, l, r, tree);
         return leftMin < rightMin ? leftMin : rightMin;
     }
 }
