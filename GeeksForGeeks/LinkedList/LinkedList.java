bc04c419ec4709607db2aefbf777d718ee40846b
Akshay Kumar <akthecoder@gmail.com>
2020-03-04 11:23:23 +0530

    Linked List inser

--- GeeksForGeeks/LinkedList/LinkedList.java
+++ GeeksForGeeks/LinkedList/LinkedList.java
@@ -3,12 +3,90 @@
  */
 public class LinkedList<T> {
   Node<T> head;
+  Node<T> end;
+  int noOfNodes;
 
   public void printList() {
     Node<T> n = head;
     while(n != null) {
       System.out.println("Node : " + n.data);
       n = n.next;
+    }
+  }
+
+  public void push(T data) {
+    Node<T> newNode = new Node<>(data);
+    if(head == null) {
+      head = newNode;
+      end = newNode;
+      return;
+    }
+      newNode.next = head;
+      head = newNode;
+  }
+
+  public void append(T data) {
+    Node<T> newNode = new Node<T>(data);
+    if(head == null) {
+      head = newNode;
+      end = newNode;
+      return;
+    }
+    end.next = newNode;
+    end  = newNode;
+  }
+  
+  public void insertAfter(T data, T insertAfter) {
+    Node<T> newNode = new Node<T>(data);
+    if(head == null) {
+      head = newNode;
+      end = newNode;
+      return;
+    }
+
+    Node<T> temp = head;
+    while(temp != null) {      
+      if(temp.data == insertAfter) {
+        Node<T> tNode = temp.next;
+        temp.next = newNode;
+        newNode.next = tNode;
+      }
+      temp = temp.next;
+    }
+    if(temp == null) {
+      System.out.println("No element found");
+      return;
+    }
+  }
+
+  public void insertBefore(T data, T insertBefore) {
+    Node<T> newNode = new Node<T>(data);
+    if(head == null) {
+      head = newNode;
+      end = newNode;
+      return;
+    }
+
+    Node<T> temp = head;
+    Node<T> prevNode = temp;
+    if(temp.data == insertBefore) {
+      Node<T> tNode = head.next;
+      head.next = newNode;
+      newNode.next = tNode;
+    }
+    temp = temp.next;
+
+    while(temp != null) {      
+      if(temp.data == insertBefore) {
+        Node<T> tNode = prevNode.next;
+        prevNode.next = newNode;
+        newNode.next = tNode;
+      }
+      temp = temp.next;
+    }
+    if(temp == null) {
+      System.out.println("No element found");
+      return;
     }
   }
 }
