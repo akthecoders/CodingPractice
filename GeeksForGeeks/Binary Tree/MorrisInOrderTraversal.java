f5fa4d3234b56695acf21059191132abbcaa42e5
Akshay Kumar <akthecoder@gmail.com>
2020-03-24 12:54:43 +0530

    MorrisInOrderTraversal

---
+++ GeeksForGeeks/Binary Tree/MorrisInOrderTraversal.java
@@ -0,0 +1,59 @@
+/**
+ * MorrisInOrderTraversal
+ */
+public class MorrisInOrderTraversal {
+    tNode root; 
+    public static void main(String[] args) {
+        MorrisInOrderTraversal tree = new MorrisInOrderTraversal(); 
+        tree.root = new tNode(1); 
+        tree.root.left = new tNode(2); 
+        tree.root.right = new tNode(3); 
+        tree.root.left.left = new tNode(4); 
+        tree.root.left.right = new tNode(5); 
+  
+        tree.MorrisTraversal(tree.root); 
+    }
+
+    public void MorrisTraversal(tNode root) {
+        tNode current, pre;
+        if(root == null) {
+            return;
+        }
+
+        current = root;
+        while(current != null) {
+            if(current.left == null) {
+                System.out.print(current.data + " ");
+                current = current.right;
+            }
+            else {
+                pre = current.left;
+                while(pre.right != null && pre.right != current) {
+                    pre = pre.right;
+                }
+
+                if(pre.right == null) {
+                    pre.right = current;
+                    current = current.left;
+                }
+                else {
+                    pre.right = null;
+                    System.out.print(current.data + " ");
+                    current = current.right;
+                }
+            }
+        }
+    }
+    
+}
+
+class tNode { 
+    int data; 
+    tNode left, right; 
+  
+    tNode(int item) 
+    { 
+        data = item; 
+        left = right = null; 
+    } 
+} 
