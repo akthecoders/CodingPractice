46ad032a448cbc3ad28e95d58b79de4552d78dbe
Akshay Kumar <akthecoder@gmail.com>
2020-04-26 23:02:23 +0530

    Minimum Distance Between BST Nodes

---
+++ LeetCode/P783.java
@@ -0,0 +1,25 @@
+// Minimum Distance Between BST Nodes
+public class P783 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    private void inorder(TreeNode root, List list) {
+        if (root != null) {
+            inorder(root.left, list);
+            list.add(root.val);
+            inorder(root.right, list);
+        }
+    }
+
+    public int minDiffInBST(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        inorder(root, list);
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < list.size() - 1; i++)
+            min = Math.min(min, list.get(i + 1) - list.get(i));
+        return min;
+    }
+}
