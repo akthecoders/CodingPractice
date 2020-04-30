22d55c8f56adf18d2e72ea51aca0487d1e83dcc5
Akshay Kumar <akthecoder@gmail.com>
2020-05-01 02:23:18 +0530

    Daily Temperatures

---
+++ LeetCode/P739.java
@@ -0,0 +1,22 @@
+// Daily Temperatures
+import java.util.*;
+public class P739 {
+    public int[] dailyTemperatures(int[] T) {
+        Stack<Integer> st = new Stack<>();
+        int result[] = new int[T.length];
+        int index = T.length - 1;
+        while (index >= 0) {
+            while (!st.isEmpty()) {
+                if (T[st.peek()] > T[index]) {
+                    result[index] = st.peek() - index;
+                    break;
+                } else {
+                    st.pop();
+                }
+            }
+            st.push(index);
+            index--;
+        }
+        return result;
+    }
+}
