1b253117b8ae3a9af64be834723493cd5281fa79
Akshay Kumar <akthecoder@gmail.com>
2020-05-01 21:56:23 +0530

    Binary Tree Preorder Traversal Updated

--- LeetCode/P144.java
+++ LeetCode/P144.java
@@ -1,6 +1,18 @@
 // Binary Tree Preorder Traversal
 import java.util.*;
 public class P144 {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> output = new ArrayList<>();
         if (root == null) {
