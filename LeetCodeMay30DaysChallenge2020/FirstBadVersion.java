99d95b40faa9018c64239e4df77db023f6a9aa80
Akshay Kumar <akthecoder@gmail.com>
2020-05-02 15:31:14 +0530

    First Bad Version

---
+++ LeetCodeMay30DaysChallenge2020/FirstBadVersion.java
@@ -0,0 +1,21 @@
+// First Bad Version
+public class FirstBadVersion {
+    public int firstBadVersion(int n) {
+        int left = 1, right = n, ans = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    public boolean isBadVersion(int n) {
+        // defined in the parent class this is the just mock.
+        return true;
+    }
+}
