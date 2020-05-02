de5b9316b9f92f8c92a05f2cccc4089c09756c6b
Akshay Kumar <akthecoder@gmail.com>
2020-05-02 15:31:49 +0530

    JewelsAndStones

---
+++ LeetCodeMay30DaysChallenge2020/JewelsAndStones.java
@@ -0,0 +1,14 @@
+// JewelsAndStones
+public class JewelsAndStones {
+    public int numJewelsInStones(String J, String S) {
+        int jewels = 0;
+        for (int i = 0; i < J.length(); i++) {
+            for (int j = 0; j < S.length(); j++) {
+                if (J.charAt(i) == S.charAt(j)) {
+                    jewels++;
+                }
+            }
+        }
+        return jewels;
+    }
+}
