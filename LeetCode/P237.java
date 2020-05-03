45803441613e7f4fda616b4ca2f3b6d0e3811493
Akshay Kumar <akthecoder@gmail.com>
2020-05-03 19:26:23 +0530

    Delete Node in a Linked List

---
+++ LeetCode/P237.java
@@ -0,0 +1,14 @@
+// Delete Node in a Linked List
+public class P237 {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
