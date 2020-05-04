cf4f57469e52d3090ecea381462d5669300d5939
Akshay Kumar <akthecoder@gmail.com>
2020-05-04 16:47:02 +0530

    Number Compliment

---
+++ LeetCodeMay30DaysChallenge2020/NumberCompliment.java
@@ -0,0 +1,9 @@
+// Number Compliment
+public class NumberCompliment {
+    public int findComplement(int num) {
+        int number_of_bits =  
+               (int)(Math.floor(Math.log(num) / 
+                             Math.log(2))) + 1; 
+        return ((1 << number_of_bits) - 1) ^ num; 
+    }
+}
