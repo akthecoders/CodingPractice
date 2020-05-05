e36e75b75cb1865ee8e3e4f52f1375d84d79f0e3
Akshay Kumar <akthecoder@gmail.com>
2020-05-05 09:18:55 +0530

    Linked List Cycle II

---
+++ LeetCode/P142.java
@@ -0,0 +1,27 @@
+// Linked List Cycle II
+public class P142 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                while (head != slow) {
+                    slow = slow.next;
+                    head = head.next;
+                }
+                return head;
+            }
+        }
+
+        return null;
+    }
+}
