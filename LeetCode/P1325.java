cec41a5389fa14970976a58a699966f52cf4fb7a
Akshay Kumar <akthecoder@gmail.com>
2020-05-09 09:43:28 +0530

    Delete Leaves With a Given Value

---
+++ LeetCode/P1325.java
@@ -0,0 +1,17 @@
+//Delete Leaves With a Given Value
+public class P1325 {
+    public TreeNode removeLeafNodes(TreeNode root, int target) {
+        if (root == null)
+            return null;
+        if (root.left == null && root.right == null && root.val == target) {
+            return null;
+        }
+        root.left = removeLeafNodes(root.left, target);
+        root.right = removeLeafNodes(root.right, target);
+
+        if (root.left == null && root.right == null && root.val == target) {
+            return null;
+        }
+        return root;
+    }
+}
