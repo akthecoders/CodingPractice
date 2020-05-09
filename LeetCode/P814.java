3069621ba3b8c2a345cd25a036da59881e80c679
Akshay Kumar <akthecoder@gmail.com>
2020-05-09 09:10:21 +0530

    Binary Tree Pruning

---
+++ LeetCode/P814.java
@@ -0,0 +1,15 @@
+// Binary Tree Pruning
+public class P814 {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null)
+            return null;
+
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+
+        if (root.left == null && root.right == null && root.val == 0)
+            return null;
+        return root;
+
+    }
+}
