aacf42e7b42cc5f6dfa6de1bca93b91d8637ed33
Akshay Kumar <akthecoder@gmail.com>
2020-05-10 16:18:46 +0530

    Fibonacci Number

---
+++ LeetCode/P509.java
@@ -0,0 +1,11 @@
+// Fibonacci Number
+public class P509 {
+    public int fib(int N) {
+        if (N == 0)
+            return 0;
+        if (N == 1)
+            return 1;
+
+        return fib(N - 1) + fib(N - 2);
+    }
+}
