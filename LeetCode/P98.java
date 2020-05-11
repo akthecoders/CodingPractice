72ad9ce4bd44dc5f7cd0687b1ad01b9bf2a31a0c
Akshay Kumar <akthecoder@gmail.com>
2020-05-11 16:23:14 +0530

    Validate Binary Search Tree

---
+++ LeetCode/P98.java
@@ -0,0 +1,15 @@
+//Validate Binary Search Tree
+public class P98 {
+    public boolean isValidBST(TreeNode root) {
+        return traverse(root, null, null);
+    }
+
+    public boolean traverse(TreeNode root, Integer min, Integer max) {
+        if (root == null)
+            return true;
+
+        if ((min != null && root.val <= min) || (max != null && root.val >= max))
+            return false;
+        return traverse(root.left, min, root.val) && traverse(root.right, root.val, max);
+    }
+}
