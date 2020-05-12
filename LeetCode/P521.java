b2cf684410c8a76b84afdd356a0f1e8f03303ced
Akshay Kumar <akthecoder@gmail.com>
2020-05-12 22:20:54 +0530

    Longest Uncommon Subsequence I

---
+++ LeetCode/P521.java
@@ -0,0 +1,8 @@
+//Longest Uncommon Subsequence I
+public class P521 {
+    public int findLUSlength(String a, String b) {
+        if (a.equals(b))
+            return -1;
+        return Math.max(a.length(), b.length());
+    }
+}
