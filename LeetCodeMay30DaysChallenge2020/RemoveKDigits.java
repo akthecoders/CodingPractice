9110e16fe8429f8b18af28a1070b9426d036e720
Akshay Kumar <akthecoder@gmail.com>
2020-05-13 16:42:40 +0530

    RemoveKDigits

---
+++ LeetCodeMay30DaysChallenge2020/RemoveKDigits.java
@@ -0,0 +1,22 @@
+public class RemoveKDigits {
+    public String removeKdigits(String num, int k) {
+        if (k >= num.length())
+            return "0";
+        StringBuilder res = new StringBuilder();
+
+        for (char c : num.toCharArray()) {
+            if (k > 0 && res.length() > 0 && res.charAt(res.length() - 1) > c) {
+                while (k > 0 && res.length() > 0 && res.charAt(res.length() - 1) > c) {
+                    res.deleteCharAt(res.length() - 1);
+                    k--;
+                }
+            }
+            res.append(c);
+        }
+        int nonZero = 0;
+        while (nonZero < res.length() && res.charAt(nonZero) == '0')
+            nonZero++;
+
+        return res.length() - k <= nonZero ? "0" : res.substring(nonZero, res.length() - k);
+    }
+}
