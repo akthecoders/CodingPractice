2ac8e001a3145437ce7f25dd3c6ace2a1b2d1e1f
Akshay Kumar <akthecoder@gmail.com>
2020-05-16 23:53:37 +0530

    Remove Duplicates from Sorted Array

---
+++ LeetCode/P26.java
@@ -0,0 +1,12 @@
+//Remove Duplicates from Sorted Array
+public class P26 {
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] != nums[i + 1]) {
+                nums[j++] = nums[i + 1];
+            }
+        }
+        return j;
+    }
+}
