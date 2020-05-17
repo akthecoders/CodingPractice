7eca86474ecd585f719e6f6dbf8b02397da4e0dc
Akshay Kumar <akthecoder@gmail.com>
2020-05-17 23:54:39 +0530

    Valid Mountain Array

---
+++ LeetCode/P941.java
@@ -0,0 +1,14 @@
+//Valid Mountain Array
+public class P941 {
+    public boolean validMountainArray(int[] A) {
+        int N = A.length;
+        int i = 0;
+        while (i + 1 < N && A[i] < A[i + 1])
+            i++;
+        if (i == 0 || i == N - 1)
+            return false;
+        while (i + 1 < N && A[i] > A[i + 1])
+            i++;
+        return i == N - 1;
+    }
+}
