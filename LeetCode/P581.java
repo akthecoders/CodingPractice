1aa82fd63d019c8e5375cc96ed03ffeb3bf3764e
Akshay Kumar <akthecoder@gmail.com>
2020-05-19 17:53:37 +0530

    Shortest Unsorted Continuous Subarray

---
+++ LeetCode/P581.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+//Shortest Unsorted Continuous Subarray
+public class P581 {
+    public int findUnsortedSubarray(int[] nums) {
+        int[] snums = nums.clone();
+        Arrays.sort(snums);
+        int start = snums.length, end = 0;
+        for (int i = 0; i < snums.length; i++) {
+            if (snums[i] != nums[i]) {
+                start = Math.min(start, i);
+                end = Math.max(end, i);
+            }
+        }
+        return (end - start >= 0 ? end - start + 1 : 0);
+    }
+}
