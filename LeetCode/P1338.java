a6e71dc9dfd206c4bcd40c47858eb21816034f34
Akshay Kumar <akthecoder@gmail.com>
2020-05-22 12:37:50 +0530

    Reduce Array Size to The Half

---
+++ LeetCode/P1338.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+//Reduce Array Size to The Half
+public class P1338 {
+    public int minSetSize(int[] arr) {
+        if (arr == null || arr.length == 0)
+            return 0;
+        int len = arr.length;
+        int target = len / 2;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            int freq = map.getOrDefault(i, 0) + 1;
+            map.put(i, freq);
+        }
+        PriorityQueue<Candidate> maxHeap = new PriorityQueue<>((o1, o2) -> Integer.compare(o2.frequency, o1.frequency));
+        for (int key : map.keySet()) {
+            maxHeap.add(new Candidate(key, map.get(key)));
+        }
+        int result = 0;
+        while (len > target) {
+            Candidate c = maxHeap.remove();
+            len -= c.frequency;
+            result++;
+        }
+
+        return result;
+    }
+
+    private static class Candidate {
+        private final int value;
+        private final int frequency;
+
+        public Candidate(int value, int frequency) {
+            this.value = value;
+            this.frequency = frequency;
+        }
+    }
+}
