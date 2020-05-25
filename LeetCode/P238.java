52d4eb595d78aa0c9abeb22639f214fe1959c6f3
Akshay Kumar <akthecoder@gmail.com>
2020-05-25 10:06:15 +0530

    Product of Array Except Self

---
+++ LeetCode/P238.java
@@ -0,0 +1,24 @@
+//Product of Array Except Self
+public class P238 {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] L = new int[n];
+        int[] R = new int[n];
+        int[] ans = new int[n];
+
+        L[0] = 1;
+        R[n - 1] = 1;
+
+        for (int i = 1; i < n; i++) {
+            L[i] = L[i - 1] * nums[i - 1];
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            R[i] = nums[i + 1] * R[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = L[i] * R[i];
+        }
+        return ans;
+    }
+}
