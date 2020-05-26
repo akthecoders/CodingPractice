cff73c27ac2a18559d98d8c3427f5770da626fbc
Akshay Kumar <akthecoder@gmail.com>
2020-05-26 21:29:57 +0530

    Beautiful Arrangement II

---
+++ LeetCode/P667.java
@@ -0,0 +1,12 @@
+//Beautiful Arrangement II
+public class P667 {
+    public int[] constructArray(int n, int k) {
+        int[] ans = new int[n];
+        int c = 0;
+        for (int v = 1; v < n - k; v++)
+            ans[c++] = v;
+        for (int i = 0; i <= k; i++)
+            ans[c++] = i % 2 == 0 ? (n - k + i / 2) : (n - i / 2);
+        return ans;
+    }
+}
