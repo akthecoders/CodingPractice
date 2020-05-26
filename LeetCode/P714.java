51a88f84bd0ecb34ca85d293b32819a43cbb784b
Akshay Kumar <akthecoder@gmail.com>
2020-05-26 22:21:40 +0530

    Best Time to Buy and Sell Stock with Transaction Fee

---
+++ LeetCode/P714.java
@@ -0,0 +1,15 @@
+//Best Time to Buy and Sell Stock with Transaction Fee
+/**
+ * P714
+ */
+public class P714 {
+    public int maxProfit(int[] prices, int fee) {
+        int cash = 0, hold = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            cash = Math.max(cash, hold + prices[i] - fee);
+            hold = Math.max(hold, cash - prices[i]);
+        }
+        return cash;
+    }
+
+}
