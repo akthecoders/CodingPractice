be1fef156d791d7aaaac7747307b8f3d9a6fdb02
Akshay Kumar <akthecoder@gmail.com>
2020-06-04 22:21:53 +0530

    Self Dividing Numbers

---
+++ LeetCode/P728.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+//Self Dividing Numbers
+public class P728 {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> ans = new ArrayList<>();
+        for (int n = left; n <= right; ++n) {
+            if (selfDividing(n))
+                ans.add(n);
+        }
+        return ans;
+    }
+
+    public boolean selfDividing(int n) {
+        for (char c : String.valueOf(n).toCharArray()) {
+            if (c == '0' || (n % (c - '0') > 0))
+                return false;
+        }
+        return true;
+    }
+}
