112613db40ad42b5753ce111a7929337470c11da
Akshay Kumar <akthecoder@gmail.com>
2020-06-12 05:25:49 +0530

    Design Twitter

--- LeetCode/P1439.java
+++ LeetCode/P1439.java
@@ -4,7 +4,7 @@ import java.util.PriorityQueue;
 //Find the Kth Smallest Sum of a Matrix With Sorted Rows
 public class P1439 {
     public int kthSmallest(int[][] mat, int k) {
-        int m = mat.length, n = mat[0].length, res = 0;
+        int m = mat.length, n = mat[0].length;
         PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         pq.offer(0);
         for (int i = 0; i < m; i++) {
