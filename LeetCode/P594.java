dea0aeb8bc7d436aec9f4b34aff57a3a4e17d8b3
Akshay Kumar <akthecoder@gmail.com>
2020-06-15 08:18:24 +0530

    Longest Harmonious Subsequence

---
+++ LeetCode/P594.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+//Longest Harmonious Subsequence
+public class P594 {
+    public int findLHS(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int res = 0;
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int key : map.keySet()) {
+            if (map.containsKey(key + 1)) {
+                res = Math.max(res, map.get(key) + map.get(key + 1));
+            }
+        }
+        return res;
+    }
+}
