66886b8853363fd36cf034d551bacb7016fcaf5a
Akshay Kumar <akthecoder@gmail.com>
2020-06-17 15:22:08 +0530

    Group Anagrams

---
+++ LeetCode/P49.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//Group Anagrams
+public class P49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0)
+            return new ArrayList<>();
+        Map<String, List<String>> ans = new HashMap<>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!ans.containsKey(key))
+                ans.put(key, new ArrayList<>());
+            ans.get(key).add(s);
+        }
+        return new ArrayList<>(ans.values());
+    }
+}
