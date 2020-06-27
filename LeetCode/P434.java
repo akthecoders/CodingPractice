57dc5393eee8d36c36c26e003197e22bb30ae723
Akshay Kumar <akthecoder@gmail.com>
2020-06-27 09:16:20 +0530

    Number of Segments in a String

---
+++ LeetCode/P434.java
@@ -0,0 +1,10 @@
+// Number of Segments in a String
+public class P434 {
+    public int countSegments(String s) {
+        String trimmed = s.trim();
+        if (trimmed.equals("")) {
+            return 0;
+        }
+        return trimmed.split("\\s+").length;
+    }
+}
