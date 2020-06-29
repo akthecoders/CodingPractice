b4ad5f4c4f32f8cbc8b19824e8a7dc5c2e242c66
Akshay Kumar <akthecoder@gmail.com>
2020-06-29 09:34:58 +0530

    Valid Palindrome

---
+++ LeetCode/P125.java
@@ -0,0 +1,13 @@
+//Valid Palindrome
+public class P125 {
+    public boolean isPalindrome(String s) {
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < s.length(); ++i) {
+            if (Character.isLetterOrDigit(s.charAt(i))) {
+                str.append(s.charAt(i));
+            }
+        }
+
+        return str.toString().toLowerCase().equals(str.reverse().toString().toLowerCase());
+    }
+}
