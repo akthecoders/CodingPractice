e856b44d690119c2e63b2fc09891d368a7304fed
Akshay Kumar <akthecoder@gmail.com>
2020-07-16 08:54:27 +0530

    First Missing Positive

---
+++ MicrosoftLeetCode/P41.java
@@ -0,0 +1,44 @@
+// First Missing Positive
+public class P41 {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        int containsOne = 0;
+        for (int x : nums) {
+            if (x == 1) {
+                containsOne = 1;
+                break;
+            }
+        }
+        if (containsOne == 0) {
+            return 1;
+        }
+
+        if (n == 1) {
+            return 2;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= 0 || nums[i] > n) {
+                nums[i] = 1;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int a = Math.abs(nums[i]);
+            if (a == n)
+                nums[0] = -Math.abs(nums[0]);
+            else
+                nums[a] = -Math.abs(nums[a]);
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > 0)
+                return i;
+        }
+
+        if (nums[0] > 0)
+            return n;
+
+        return n + 1;
+    }
+}
