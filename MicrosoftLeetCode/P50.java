562b768d7874e172ce3d0f7b46f9d4ac1f0dd332
Akshay Kumar <akthecoder@gmail.com>
2020-07-22 20:19:40 +0530

    Pow(x, n)

---
+++ MicrosoftLeetCode/P50.java
@@ -0,0 +1,19 @@
+//Pow(x, n)
+public class P50 {
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double ans = 1;
+        double currP = x;
+        for (long i = N; i > 0; i /= 2) {
+            if ((i % 2) == 1) {
+                ans *= currP;
+            }
+            currP *= currP;
+        }
+        return ans;
+    }
+}
