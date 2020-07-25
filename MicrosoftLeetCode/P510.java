ee1dbe36aaad415407c5e6a6136d315089473d86
Akshay Kumar <akthecoder@gmail.com>
2020-07-25 12:47:42 +0530

    Inorder Successor in BST II

---
+++ MicrosoftLeetCode/P510.java
@@ -0,0 +1,21 @@
+// Inorder Successor in BST II
+public class P510 {
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node parent;
+    };
+    public Node inorderSuccessor(Node node) {
+        if (node.right != null) {
+            node = node.right;
+            while (node.left != null)
+                node = node.left;
+            return node;
+        }
+
+        while (node.parent != null && node == node.parent.right)
+            node = node.parent;
+        return node.parent;
+    }
+}
