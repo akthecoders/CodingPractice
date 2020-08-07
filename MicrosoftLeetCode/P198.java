f9a13fc8e70f6b30a851516b51aff0b5235228eb
Akshay Kumar <akthecoder@gmail.com>
2020-08-07 15:58:23 +0530

    House Robber

---
+++ MicrosoftLeetCode/P198.java
@@ -0,0 +1,13 @@
+//House Robber
+public class P198 {
+    public int rob(int[] nums) {
+        int prevMax = 0;
+        int currMax = 0;
+        for (int x : nums) {
+            int temp = currMax;
+            currMax = Math.max(prevMax + x, currMax);
+            prevMax = temp;
+        }
+        return currMax;
+    }
+}
