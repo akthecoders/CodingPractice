530e69ba8da597bad15bcfa3232f1d168cf6c92f
Akshay Kumar <akthecoder@gmail.com>
2020-08-07 19:05:35 +0530

    Invert Binary Tree

---
+++ MicrosoftLeetCode/P226.java
@@ -0,0 +1,36 @@
+//Invert Binary Tree
+public class P226 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null)
+            return null;
+        if (root.left == null && root.right == null)
+            return root;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
