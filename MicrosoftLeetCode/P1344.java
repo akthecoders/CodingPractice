913b2fabec0a6d1408ebfa653619d2900c1d9a60
Akshay Kumar <akthecoder@gmail.com>
2020-08-07 14:32:52 +0530

    Angle Between Hands of a Clock

---
+++ MicrosoftLeetCode/P1344.java
@@ -0,0 +1,13 @@
+//Angle Between Hands of a Clock
+public class P1344 {
+    public double angleClock(int hour, int minutes) {
+        int oneMinAngle = 6;
+        int oneHourAngle = 30;
+
+        double minutesAngle = oneMinAngle * minutes;
+        double hourAngle = (hour % 12 + minutes / 60.0) * oneHourAngle;
+
+        double diff = Math.abs(hourAngle - minutesAngle);
+        return Math.min(diff, 360 - diff);
+    }
+}
