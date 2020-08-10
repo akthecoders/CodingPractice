63e8ddc99236548ea13e1aa081055659d581c21d
Akshay Kumar <akthecoder@gmail.com>
2020-08-10 11:55:52 +0530

    Jump Game

---
+++ MicrosoftLeetCode/P55.java
@@ -0,0 +1,12 @@
+//Jump Game
+public class P55 {
+    public boolean canJump(int[] nums) {
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
+}
