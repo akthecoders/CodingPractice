8880eb07f7d59aba4e33568231f3c9adb5536673
Akshay Kumar <akthecoder@gmail.com>
2020-08-12 10:06:05 +0530

    Two Sum IV - Input is a BST

---
+++ MicrosoftLeetCode/P653.java
@@ -0,0 +1,44 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+//Two Sum IV - Input is a BST
+public class P653 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean findTarget(TreeNode root, int k) {
+        Set<Integer> set = new HashSet<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            if (queue.peek() != null) {
+                TreeNode node = queue.remove();
+                if (set.contains(k - node.val))
+                    return true;
+                set.add(node.val);
+                queue.add(node.right);
+                queue.add(node.left);
+            } else
+                queue.remove();
+        }
+        return false;
+    }
+}
