eade059266df8aa168e9190925739152e1959aee
Akshay Kumar <akthecoder@gmail.com>
2020-08-14 09:46:24 +0530

    Backspace String Compare

---
+++ MicrosoftLeetCode/P844.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+//Backspace String Compare
+public class P844 {
+    public boolean backspaceCompare(String S, String T) {
+        return build(S).equals(build(T));
+    }
+
+    public String build(String S) {
+        Stack<Character> ans = new Stack<>();
+        for (char c : S.toCharArray()) {
+            if (c != '#')
+                ans.push(c);
+            else if (!ans.empty())
+                ans.pop();
+        }
+        return String.valueOf(ans);
+    }
+}
