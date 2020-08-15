164967f4b042e3fcedc6fcf314bdf39e6fef37fd
Akshay Kumar <akthecoder@gmail.com>
2020-08-15 22:40:52 +0530

    K-diff Pairs in an Array

---
+++ MicrosoftLeetCode/P532.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+//K-diff Pairs in an Array
+public class P532 {
+    public int findPairs(int[] nums, int k) {
+        int result = 0;
+        HashMap<Integer, Integer> counter = new HashMap<>();
+
+        for (int n : nums)
+            counter.put(n, counter.getOrDefault(n, 0) + 1);
+
+        for (Map.Entry<Integer, Integer> entry : counter.entrySet()) {
+            int x = entry.getKey();
+            int val = entry.getValue();
+            if (k > 0 && counter.containsKey(x + k)) {
+                result++;
+            } else if (k == 0 && val > 1) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
