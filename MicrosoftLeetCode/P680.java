1b1ba3512d5550c6062e1cba1a2e46b713ebb04f
Akshay Kumar <akthecoder@gmail.com>
2020-08-15 16:11:46 +0530

    Valid Palindrome II

---
+++ MicrosoftLeetCode/P680.java
@@ -0,0 +1,20 @@
+//Valid Palindrome II
+public class P680 {
+    public boolean validPalindrome(String s) {
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                int j = s.length() - 1 - i;
+                return isPalindromeRange(s, i + 1, j) || isPalindromeRange(s, i, j - 1);
+            }
+        }
+        return true;
+    }
+
+    public boolean isPalindromeRange(String s, int i, int j) {
+        for (int k = i; k <= i + (j - i) / 2; k++) {
+            if (s.charAt(k) != s.charAt(j - k + i))
+                return false;
+        }
+        return true;
+    }
+}
