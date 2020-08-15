dbf72fb48b6aa2035288e9dd722e316e582133cf
Akshay Kumar <akthecoder@gmail.com>
2020-08-15 22:48:52 +0530

    Excel Sheet Column Title

---
+++ MicrosoftLeetCode/P168.java
@@ -0,0 +1,16 @@
+// Excel Sheet Column Title
+public class P168 {
+    public String convertToTitle(int n) {
+        if (n <= 0)
+            return "";
+        StringBuilder sb = new StringBuilder();
+
+        while (n > 0) {
+            n--;
+            sb.append((char) ('A' + n % 26));
+            n /= 26;
+        }
+        sb.reverse();
+        return sb.toString();
+    }
+}
