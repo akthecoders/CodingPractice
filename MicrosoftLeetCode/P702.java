7bfb2c373593c4ee7c0b20ef0446b71a6c34749b
Akshay Kumar <akthecoder@gmail.com>
2020-08-18 11:55:10 +0530

    Search in a Sorted Array of Unknown Size

---
+++ MicrosoftLeetCode/P702.java
@@ -0,0 +1,31 @@
+//Search in a Sorted Array of Unknown Size
+public class P702 {
+    interface ArrayReader {
+        public int get(int index);
+    }
+    public int search(ArrayReader reader, int target) {
+        if (reader.get(0) == target)
+            return 0;
+
+        int left = 0;
+        int right = 1;
+        while (reader.get(right) < target) {
+            left = right;
+            right <<= 1;
+        }
+
+        int pivot, num;
+
+        while (left <= right) {
+            pivot = left + ((right - left) / 2);
+            num = reader.get(pivot);
+            if (num == target)
+                return pivot;
+            if (num > target)
+                right = pivot - 1;
+            else
+                left = pivot + 1;
+        }
+        return -1;
+    }
+}
