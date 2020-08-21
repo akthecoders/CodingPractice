e1502e9a34adcf75f8aa76202e9232f3bc3454c1
Akshay Kumar <akshay@akthecoder.com>
2020-08-21 10:35:53 +0530

    Inser delete random

---
+++ MicrosoftLeetCode/P380.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+//Insert Delete GetRandom O(1)
+public class P380 {
+    Map<Integer, Integer> dict;
+    List<Integer> list;
+    Random rand = new Random();
+
+    /** Initialize your data structure here. */
+    public P380() {
+        dict = new HashMap<>();
+        list = new ArrayList<>();
+    }
+
+    /**
+     * Inserts a value to the set. Returns true if the set did not already contain
+     * the specified element.
+     */
+    public boolean insert(int val) {
+        if (dict.containsKey(val))
+            return false;
+        dict.put(val, list.size());
+        list.add(list.size(), val);
+        return true;
+    }
+
+    /**
+     * Removes a value from the set. Returns true if the set contained the specified
+     * element.
+     */
+    public boolean remove(int val) {
+        if (!dict.containsKey(val))
+            return false;
+        Integer lastElement = list.get(list.size() - 1);
+        int idx = dict.get(val);
+        list.set(idx, lastElement);
+        dict.put(lastElement, idx);
+
+        list.remove(list.size() - 1);
+        dict.remove(val);
+        return true;
+    }
+
+    /** Get a random element from the set. */
+    public int getRandom() {
+        return list.get(rand.nextInt(list.size()));
+    }
+}
