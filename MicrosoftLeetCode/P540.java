d2bb46687033a37d2d8d5e1a5d5fd1e860d3262c
Akshay Kumar <akthecoder@gmail.com>
2020-08-23 18:13:28 +0530

    Single Element in a Sorted Array

---
+++ MicrosoftLeetCode/P540.java
@@ -0,0 +1,18 @@
+//Single Element in a Sorted Array
+public class P540 {
+    public int singleNonDuplicate(int[] nums) {
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (mid % 2 == 1)
+                mid--;
+            if (nums[mid] == nums[mid + 1]) {
+                lo = mid + 2;
+            } else {
+                hi = mid;
+            }
+        }
+        return nums[lo];
+    }
+}
