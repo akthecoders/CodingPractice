3bcaeb6e8ee1b01d63517de9bc45ade4e4f54b3c
Akshay Kumar <akshay@akthecoder.com>
2020-08-26 11:41:21 +0530

    Longest Consecutive Sequence

---
+++ MicrosoftLeetCode/P128.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
+
+//Longest Consecutive Sequence
+public class P128 {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : numSet) {
+            if (!numSet.contains(num - 1)) {
+                int currentNum = num;
+                int currStreak = 1;
+
+                while (numSet.contains(currentNum + 1)) {
+                    currentNum++;
+                    currStreak++;
+                }
+                longestStreak = Math.max(longestStreak, currStreak);
+            }
+        }
+        return longestStreak;
+    }
+}
