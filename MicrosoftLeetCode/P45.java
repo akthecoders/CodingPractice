68e4418f4e748caed77b5c6bc836eafc5ca03630
Akshay Kumar <akshay@akthecoder.com>
2020-08-28 16:52:49 +0530

    Jump Game II

--- MicrosoftLeetCode/P45.java
+++ MicrosoftLeetCode/P45.java
@@ -1,4 +1,21 @@
 // Jump Game II
 public class P45 {
-    
+    public int jump(int[] nums) {
+        int n = nums.length;
+        if(n < 2) return 0;
+        
+        int maxPos = nums[0];
+        int maxSteps = nums[0];
+        
+        int jumps = 1;
+        
+        for(int i = 1; i < n; i++) {
+            if(maxSteps < i) {
+                jumps++;
+                maxSteps = maxPos;
+            }
+            maxPos = Math.max(maxPos, nums[i] + i);
+        }
+        return jumps;
+    }
 }
