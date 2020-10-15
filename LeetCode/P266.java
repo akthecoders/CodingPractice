91388ffac8ebe853fd4c41346b2de4c251d5518e
Akshay Kumar <akshay@akthecoder.com>
2020-10-15 09:35:31 +0530

    Palindrome Permutation

---
+++ LeetCode/P266.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+//Palindrome Permutation
+public class P266 {
+    public boolean canPermutePalindrome(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        int count = 0;
+        for (char key : map.keySet()) {
+            count += map.get(key) % 2;
+        }
+        return count <= 1;
+    }
+}
