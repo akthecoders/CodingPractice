4fc2df4de07690a6800ef4b1d8f08893d25e5c7c
akthecoders <akshay@akthecoder.com>
2020-11-24 21:24:15 +0530

    P2015

---
+++ MicrosoftLeetCode/P2015.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+//Isomorphic Strings
+public class P2015 {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character, Character> map = new HashMap<>();
+        if (s.length() != t.length())
+            return false;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            char ch2 = t.charAt(i);
+
+            if (map.containsKey(ch)) {
+                if (map.get(ch) != ch2)
+                    return false;
+            } else {
+                if (map.containsValue(ch2))
+                    return false;
+                map.put(ch, ch2);
+            }
+        }
+        return true;
+    }
+}
